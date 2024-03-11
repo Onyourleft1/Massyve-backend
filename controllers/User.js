2fc12f28686eb9beb6d393119a84244c308769a2
OnYourLeft1 <fadiajami82@gmail.com>
2024-03-11 16:12:26 +0200

    Test2

--- controllers/User.js
+++ controllers/User.js
@@ -2,4 +2,7 @@ module.exports = {
   get: async (req, res) => {
     return res.status(200).json("Hello");
   },
+  create: async (req, res) => {
+    return res.status(200).json(test);
+  },
 };
--- routes/User.js
+++ routes/User.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/User");
 
 router.get("/get", controller.get);
+router.get("/create", controller.create);
 
 module.exports = router;
